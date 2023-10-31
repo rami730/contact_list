bd6e4e6a564661a16d0a70f86d1fd91b38f97ec4
Rami Abumeghafel <rami_730@hotmail.com>
2023-10-31 17:58:31 +0100

    Added new methods, changed to address and phone to lists

--- Program.cs
+++ Program.cs
@@ -2,31 +2,30 @@
 {
     class MainClass
     {
-        //static Person[] contactList = new Person[100];
         static List<Person> contactList = new List<Person>();
         class Person
         {
             public string persname, surname, birthdate;
-            public string[] phone = new string[100];
-            public string[] address = new string[100];
+
+            public List<string> phone = new List<string>();
+            public List<string> address = new List<string>();
             
             public void PrintInfo()
             {
                 Console.Write($"Name: {this.persname} {this.surname}, Phone number(s): ");
-                for(int i = 0; i < this.phone.Length; i++)
+                for(int i = 0; i < this.phone.Count; i++)
                 {
                     if (this.phone[i] != null)
                         Console.Write(this.phone[i] + ", ");
                 }
                 Console.Write("Address(es): ");
-                for(int i = 0; i < this.address.Length; i++)
+                for(int i = 0; i < this.address.Count; i++)
                 {
                     if (this.address[i] != null)
                         Console.Write(this.address[i] + ", ");
                 }
                 Console.Write($"Birthdate: {this.birthdate} \n");
             }
-
         }
         static string[] Input(string s)
         {
@@ -51,6 +50,7 @@
             using (StreamReader infile = new StreamReader(lastFileName))
             {
                 string line;
+                contactList.Clear();
                 while ((line = infile.ReadLine()) != null)
                 {
                     string[] attrs = line.Split('|'); //name | surname | p1, p2.. pn | adr1, adr2... adrn |
@@ -60,12 +60,12 @@
                     string[] phones = attrs[2].Split(';');
                     for (int i = 0; i < phones.Length; i++)
                     {
-                        p.phone[i] = phones[i];
+                        p.phone.Add(phones[i]);
                     }
                     string[] addresses = attrs[3].Split(';');
                     for (int i = 0; i < addresses.Length; i++)
                     {
-                        p.address[i] = addresses[i];
+                        p.address.Add(addresses[i]);
                     }
                     contactList.Add(p);
                 }
@@ -73,6 +73,7 @@
         }
         private static void NewPerson(List<string> tempName)
         {
+            string[] tempString;
             Person p = new Person();
             if (tempName.Count == 0)
             {
@@ -87,12 +88,34 @@
                 p.surname = tempName[1];
             }
             Console.Write("phone: ");
-            p.phone = Console.ReadLine().Split(';');
+            tempString = Console.ReadLine().Split(';');
+            foreach (string phone in tempString)
+            {
+                p.phone.Add(phone);
+            }
             Console.Write("address: ");
-            p.address = Console.ReadLine().Split(';');
+            tempString = Console.ReadLine().Split(';');
+            foreach (string address in tempString)
+            {
+                p.address.Add(address);
+            }
             Console.Write("birthdate: ");
             p.birthdate = Console.ReadLine();
             contactList.Add(p);
+        }
+        private static void Save(string lastFileName)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(lastFileName))
+            {
+                foreach (Person p in contactList)
+                {
+                    string joinedNumbers = string.Join(";", p.phone);
+                    string joinedAddresses = string.Join(";", p.address);
+                    streamWriter.Write($"{p.persname}|{p.surname}|{joinedNumbers}|{joinedAddresses}|");
+                    streamWriter.Write(p.birthdate + '\n');
+                }
+                streamWriter.Close();
+            }
         }
         public static void Main(string[] args)
         {
@@ -123,28 +146,19 @@
                 }
                 else if (commandLine[0] ==  "list")
                 {
-                    foreach(Person p in contactList)
-                    {
-                        p.PrintInfo();
-                    }
-                }
-                else if (commandLine[0] == "save")
-                {
                     if (commandLine.Length < 2)
                     {
-                        using (StreamWriter outfile = new StreamWriter(lastFileName))
+                        foreach (Person p in contactList)
                         {
-                            foreach (Person p in contactList)
-                            {
-                                if (p != null)
-                                    outfile.WriteLine($"{p.persname};{p.surname};{p.phone};{p.address};{p.birthdate}");
-                            }
+                            p.PrintInfo();
                         }
                     }
                     else
                     {
-                        // NYI!
-                        Console.WriteLine("Not yet implemented: save /file/");
+                        foreach(Person p in contactList)
+                        {
+                            if (p.persname == commandLine[1] && p.surname == commandLine[2]) p.PrintInfo();
+                        }
                     }
                 }
                 else if (commandLine[0] == "new")
@@ -156,7 +170,30 @@
                         tempName.Add(commandLine[2]);
                     }
                     NewPerson(tempName);
-
+                }
+                else if (commandLine[0] == "delete")
+                {
+                    if(commandLine.Length < 2)
+                    {
+                        contactList.Clear();
+                    }
+                    else
+                    {
+                        for (int i = 0; i < contactList.Count; i++)
+                        {
+                            if (contactList[i].persname == commandLine[1] && contactList[i].surname == commandLine[2])
+                            {
+                                contactList.RemoveAt(i);
+                            }
+                        }
+                    }
+                }
+                else if (commandLine[0] == "save")
+                {
+                    if (commandLine.Length < 2)
+                        Save(lastFileName);
+                    else
+                        Save(commandLine[1]);
                 }
                 else if (commandLine[0] == "help")
                 {
